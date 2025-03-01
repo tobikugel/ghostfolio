f38945270e5303968ca61400fd3de56a190b3475
tobikugel <tobiaskugel@outlook.com>
2025-03-01 17:32:36 -0300

    first commit before testing

--- apps/client/src/app/pages/register/show-access-token-dialog/show-access-token-dialog.module.ts
+++ apps/client/src/app/pages/register/show-access-token-dialog/show-access-token-dialog.module.ts
@@ -5,10 +5,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 import { ShowAccessTokenDialog } from './show-access-token-dialog.component';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @NgModule({
   declarations: [ShowAccessTokenDialog],
@@ -21,7 +24,24 @@ import { ShowAccessTokenDialog } from './show-access-token-dialog.component';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    TextFieldModule
+    TextFieldModule,
+    MatStepperModule,
+    MatCheckboxModule,
+    MatIconModule
+  ],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {
+        displayDefaultIndicatorType: false
+      }
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        subscriptSizing: 'dynamic'
+      }
+    }
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
