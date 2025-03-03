82739e4be2cac41d79507b6d3f09a62b2d956d65
tobikugel <tobiaskugel@outlook.com>
2025-03-03 10:44:37 -0300

    Adjusted text and buttons from feedback

--- apps/client/src/app/pages/register/show-access-token-dialog/show-access-token-dialog.module.ts
+++ apps/client/src/app/pages/register/show-access-token-dialog/show-access-token-dialog.module.ts
@@ -1,17 +1,21 @@
 import { ClipboardModule } from '@angular/cdk/clipboard';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { TextFieldModule } from '@angular/cdk/text-field';
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldModule
+} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatIconModule} from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+
 import { ShowAccessTokenDialog } from './show-access-token-dialog.component';
-import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @NgModule({
   declarations: [ShowAccessTokenDialog],
